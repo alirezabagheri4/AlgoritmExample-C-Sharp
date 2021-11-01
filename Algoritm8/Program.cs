6303e907e29f1679acab86e51ec5fc27b312ca22
Alireza Bagheri <78638285+alirezabagheri4@users.noreply.github.com>
2021-11-01 15:51:56 +0330

    .

---
+++ Algoritm8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please inter number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int result=Fibunachi(input);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        private static int Fibunachi(int n)
+        {
+            if (n > 2)
+            {
+                return Fibunachi(n - 1) + Fibunachi(n - 2);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
