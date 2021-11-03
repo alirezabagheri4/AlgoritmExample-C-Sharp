e2a6184c5cecf87a656c91efec791fe241c23b0a
Alireza Bagheri <78638285+alirezabagheri4@users.noreply.github.com>
2021-11-03 12:04:05 +0330

    .

--- Algoritm8/Program.cs
+++ Algoritm8/Program.cs
@@ -6,10 +6,22 @@ namespace Algoritm8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please inter number");
+            //Console.WriteLine("Please inter number");
+            //int input = Convert.ToInt32(Console.ReadLine());
+            //int result=Fibunachi(input);
+            //Console.WriteLine(result);
+            //Console.ReadKey();
+
+            Console.WriteLine("Please inter number h");
             int input = Convert.ToInt32(Console.ReadLine());
-            int result=Fibunachi(input);
-            Console.WriteLine(result);
+            //int result=Fibunachi(input);
+            //Console.WriteLine(result);
+            var sum = 0;
+            for (var i = 1; i < input; i++)
+            {
+
+            }
+            Console.WriteLine(sum);
             Console.ReadKey();
         }
 
